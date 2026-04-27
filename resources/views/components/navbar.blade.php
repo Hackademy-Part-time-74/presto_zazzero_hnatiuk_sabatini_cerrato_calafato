2bc16bb0d14c94ce795bd0658e952ab971b35fae
Roberto Sabatini <robertosabatini07@gmail.com>
2026-04-27 22:12:17 +0200

    Aggiunta pagina crea articolo con Livewire se autenticato

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -3,26 +3,29 @@
                     {{--Logo--}}
                 </svg> </a> </div>
         <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-            <li><a href="#" class="nav-link px-2 link-secondary">Home</a></li>
+            <li><a href="/" class="nav-link px-2 link-secondary">Home</a></li>
             <li><a href="#" class="nav-link px-2">Features</a></li>
             <li><a href="#" class="nav-link px-2">Pricing</a></li>
             <li><a href="#" class="nav-link px-2">FAQs</a></li>
             <li><a href="#" class="nav-link px-2">About</a></li>
         </ul>
        @auth  
-       <p>Ciao, {{ auth()->user()->name }}</p>
-       <form action="{{ route('logout')}}" method="POST" id="form-logout">
+        <p class="mb-0">Ciao, {{ auth()->user()->name }}</p>
+        <ul>
+            <li><a href="{{ route('create.article') }}" class="nav-link px-2">Crea Articolo</a></li>
+        </ul>
+        <form action="{{ route('logout')}}" method="POST" id="form-logout">
        @csrf 
        <button type="submit" class="btn btn-sm btn-danger">Esci</button>
        </form>
        @else   
         <div class="col-md-3 text-end"> 
-           <button  type="button" class="btn btn-outline-primary me-2">
-            <a href="{{ route('login')}}"> Login</a>
-        </button>
-           <button type="button" class="btn btn-primary">
-            <a href="{{ route('register')}}"> Registrati</a>
-        </button> 
+            <button  type="button" class="btn btn-outline-primary me-2">
+                <a href="{{ route('login')}}"> Login</a>
+            </button>
+            <button type="button" class="btn btn-primary">
+                <a href="{{ route('register')}}"> Registrati</a>
+            </button> 
         </div>
         @endauth
 </header>
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\PublicController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [ PublicController::class, 'homepage'])->name('homepage');
+
+Route::get('/create/article',[ArticleController::class,'create'])->name('create.article');
