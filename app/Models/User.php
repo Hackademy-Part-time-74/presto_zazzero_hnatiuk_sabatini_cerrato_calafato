cb686911886332cd2d76373f95fa7d54aa284073
Kevin Zazzero <kevin.zazzero@hotmail.it>
2026-04-27 20:41:07 +0200

    Creati model user category article

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -46,4 +47,8 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+    public function articles() : HasMany{
+        return $this->HasMany(Article::class);
+    }
 }
+
