c831b345a8b67e4afb21a6a63d86535b931afb6a
carlo_calafato <carlocalafato3@gmail.com>
2026-04-30 21:02:31 +0200

    User story 3 completata

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,24 +1,24 @@
 <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-    
-    @if(!auth()->user()->is_revisor)
-    <div class="col-md-5 offset-md-1 mb-3 text-center">
-        <h5>Vuoi diventare revisore?</h5>
-        <p>Cliccando il bottone sottostante farai richiesta ai nostri admin</p>
-        <div class="d-flex flex-column align-items-center ">
+    @auth
+        @if(!auth()->user()->is_revisor)
+        <div class="col-md-5 offset-md-1 mb-3 text-center">
+            <h5>Vuoi diventare revisore?</h5>
+            <p>Cliccando il bottone sottostante farai richiesta ai nostri admin</p>
+            <div class="d-flex flex-column align-items-center ">
 
-        <div class="container-fluid justify-content-around">
-            <img src="{{ asset('images/carlo.jpeg') }}" alt="Il nostro Sofficino <3" height="48" width="48" class="rounded-5 mb-3">
-            <img src="{{ asset('images/lisa.jpg') }}" alt="La nostra RobyAmatoRevisore <3" height="48" width="48" class="rounded-5 mb-3">
-            <img src="{{ asset('images/roby.jpeg') }}" alt="Il nostro RobyAmatoRevisore <3" height="48" width="48" class="rounded-5 mb-3">
-            <img src="{{ asset('images/iva.jpeg') }}" alt="La nostra amata IvaArrabbiata <3" height="48" width="48" class="rounded-5 mb-3">
-            <img src="{{ asset('images/kevin.jpeg') }}" alt="Il nostro amato KevinSeeder <3" height="48" width="48" class="rounded-5 mb-3">
+            <div class="container-fluid justify-content-around">
+                <img src="{{ asset('images/carlo.jpeg') }}" alt="Il nostro Sofficino <3" height="48" width="48" class="rounded-5 mb-3">
+                <img src="{{ asset('images/lisa.jpg') }}" alt="La nostra RobyAmatoRevisore <3" height="48" width="48" class="rounded-5 mb-3">
+                <img src="{{ asset('images/roby.jpeg') }}" alt="Il nostro RobyAmatoRevisore <3" height="48" width="48" class="rounded-5 mb-3">
+                <img src="{{ asset('images/iva.jpeg') }}" alt="La nostra amata IvaArrabbiata <3" height="48" width="48" class="rounded-5 mb-3">
+                <img src="{{ asset('images/kevin.jpeg') }}" alt="Il nostro amato KevinSeeder <3" height="48" width="48" class="rounded-5 mb-3">
+            </div>
+
+            <a href="{{ route('become.revisor') }}" class="btn btn-warning w-25">Diventa Revisore</a>
+            </div>
         </div>
-
-        <a href="{{ route('become.revisor') }}" class="btn btn-warning w-25">Diventa Revisore</a>
-        </div>
-    </div>
-    @endif
-
+        @endif
+    @endauth
     <div class="col-md-4 d-flex align-items-center"> 
         <a href="/" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1" aria-label="Bootstrap"> 
             <svg class="bi" width="30" height="24" aria-hidden="true">
