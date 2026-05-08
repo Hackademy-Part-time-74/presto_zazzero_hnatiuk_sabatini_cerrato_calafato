dd5ad277350c3dd3e596588aee0dd4051aea0bf8
Ivanna <iwanozka666@gmail.com>
2026-05-08 21:19:07 +0200

    User Story 5 completata

--- resources/views/article/show.blade.php
+++ resources/views/article/show.blade.php
@@ -9,25 +9,18 @@
         </div>
         <div class="row height-custom justify-content-center py-5">
             <div class="col-12 col-md-6 mb-3">
+             @if ($article->images->count() > 0)
                 <div id="carouselExample" class="carousel slide">
                     <div class="carousel-inner">
-                        <div class="carousel-item active">
+                        @foreach($article->images as $key => $image)
+                        <div class="carousel-item @if($loop->first) active @endif">
 
-                            <img src="https://picsum.photos/400" class="d-block w-100 rounded shadow" alt="...">
-
-                        </div>
-                        <div class="carousel-item">
-
-                            <img src="https://picsum.photos/402" class="d-block w-100 rounded shadow" alt="...">
+                            <img src="{{ Storage::url($image->path) }}" class="d-block w-100 rounded shadow" alt="Immagine {{ $key + 1 }} dell'articolo {{ $article->title }}">
 
                         </div>
-                        <div class="carousel-item">
-
-                            <img src="https://picsum.photos/401" class="d-block w-100 rounded shadow" alt="...">
-
-                        </div>
+                        @endforeach
                     </div>
-
+                       @if($article->images->count() > 1)
                     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
                         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                         <span class="visually-hidden">Previous</span>
@@ -37,8 +30,11 @@
                         <span class="carousel-control-next-icon" aria-hidden="true"></span>
                         <span class="visually-hidden">Next</span>
                     </button>
-
+                      @endif
                 </div>
+             @else
+             <img src="https://picsum.photos/400" alt="Nessuna foto inserita dall'utente">
+             @endif
             </div>
             <div class="col-12 col-md-6 mb-3 height-custom text-center shadow-lg p-5 rounded border border-2 border-warning">
 
