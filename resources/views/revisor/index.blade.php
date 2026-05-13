ffe8337bddd2d423f7035a1dab4ae70372a74865
carlo_calafato <carlocalafato3@gmail.com>
2026-05-13 22:21:33 +0200

    sistemata crop nella view revisor/index

--- resources/views/revisor/index.blade.php
+++ resources/views/revisor/index.blade.php
@@ -27,7 +27,8 @@
                     @if($article_to_check->images->count())
                     @foreach ($article_to_check->images as $key=>$image)
                         <div class="col-6 col-md-4 mb-4">
-                            <img src="{{ $image->getUrl(300,300) }}" class="img-fluid rounded shadow" alt="immagine {{ $key +1 }} dell'articolo '{{ $article_to_check->title }}">
+                            <img src="{{ $article_to_check->images->isNotEmpty() ? $article_to_check->images->first()->getUrl(300, 300) : 'https://picsum.photos/300'}}"
+                                    class="card-img-top" alt="immagine articolo {{ $article_to_check->title }}">
                         </div>
                     @endforeach
                 @else
