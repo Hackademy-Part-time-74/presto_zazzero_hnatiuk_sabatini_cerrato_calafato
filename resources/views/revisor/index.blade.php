04b446137f00255ee74ed38672e44b9022ba9d49
Ivanna <iwanozka666@gmail.com>
2026-05-16 12:15:08 +0200

    Modificato revisor/index layout

--- resources/views/revisor/index.blade.php
+++ resources/views/revisor/index.blade.php
@@ -21,65 +21,78 @@
 
 
         <div class="row justify-content-center pt-5">
-            <div class="col-md-8">
+            <div class="col-md-6">
                 <div class="row justify-content-center">
         @if($article_to_check)
                     @if($article_to_check->images->count())
-                    @foreach ($article_to_check->images as $key=>$image)
-                        <div class="col-6 col-md-4 mb-4">
-                            <img src="{{ $article_to_check->images->isNotEmpty() ? $image->getUrl(300, 300) : 'https://picsum.photos/300'}}"
-                                    class="card-img-top" alt="immagine articolo {{ $article_to_check->title }}">
-                        </div>
-                        <div class="col-md-8">
-                            <div class="card-body">
-                                <h5>Labels</h5>
-                                @if ($image->labels)
-                                    @foreach ($image->labels as $label)
-                                        #{{ $label }},
-                                    @endforeach
-                                @else
-                                    <p class="fst-italic">No labels</p>
+                    @foreach ($article_to_check->images as $key => $image)
+                        <div class="col-12">
+                            <div class="card mb-3">
+                                <div class="row">
+                                    <div class="col-md-6  d-flex justify-content-center align-items-center">
+                                        <img 
+                                src="{{ $image->getUrl(300, 300) }}"
+                                alt="Immagine {{ $key + 1 }} dell'articolo {{ $article_to_check->title }}"
+                                class="img-fluid rounded shadow">
+                                    </div>
 
-                                @endif
-                                <h5>Ratings</h5>
-                                <div class="row justify-content-center">
-                                    <div class="col-2">
-                                        <div class="text-center mx-auto {{ $image->adult }}"></div>
-                                        <div class="col-10">Adult</div>
+                                    <div class="col-md-3">
+                                        <div class="card-body">
+                                            <h5>Labels</h5>
+                                            @if ($image->labels)
+                                            @foreach ($image->labels as $label)
+                                                #{{ $label }},
+                                            @endforeach
+                                            @else
+                                            <p class="fst-italic">No labels</p>
+                                            @endif
+                                        </div>
+                                    </div>
+
+                                    <div class="col-md-3">
+                                        <div class="card-body">
+                                            <h5>Google Vision Ratings</h5>
+                                            <div class="row justify-content-center">
+                                                <div class="col-2">
+                                                    <div class="text-center mx-auto {{ $image->adult }}"></div>
+                                                </div>
+                                                <div class="col-10">adult</div>
+                                            </div>
+
+                                            <div class="row justify-content-center">
+                                                <div class="col-2">
+                                                    <div class="text-center mx-auto {{ $image->violence }}"></div>
+                                                </div>
+                                                <div class="col-10">violence</div>
+                                            </div>
+                                            
+                                            <div class="row justify-content-center">
+                                                <div class="col-2">
+                                                    <div class="text-center mx-auto {{ $image->spoof }}"></div>
+                                                </div>
+                                                <div class="col-10">spoof</div>
+                                            </div>
+
+                                            <div class="row justify-content-center">
+                                                <div class="col-2">
+                                                    <div class="text-center mx-auto {{ $image->racy }}"></div>
+                                                </div>
+                                                <div class="col-10">racy</div>
+                                            </div>
+
+                                            <div class="row justify-content-center">
+                                                <div class="col-2">
+                                                    <div class="text-center mx-auto {{ $image->medical }}"></div>
+                                                </div>
+                                                <div class="col-10">medical</div>
+                                            </div>
+                                            
+                                        </div>
                                     </div>
                                 </div>
-
-                                <div class="row justify-content-center">
-                                    <div class="col-2">
-                                        <div class="text-center mx-auto {{ $image->violence }}"></div>
-                                        <div class="col-10">Violence</div>
-                                    </div>
-                                </div>
-
-                                <div class="row justify-content-center">
-                                    <div class="col-2">
-                                        <div class="text-center mx-auto {{ $image->spoof }}"></div>
-                                        <div class="col-10">Spoof</div>
-                                    </div>
-                                </div>
-
-                                <div class="row justify-content-center">
-                                    <div class="col-2">
-                                        <div class="text-center mx-auto {{ $image->racy }}"></div>
-                                        <div class="col-10">Racy</div>
-                                    </div>
-                                </div>
-
-                                <div class="row justify-content-center">
-                                    <div class="col-2">
-                                        <div class="text-center mx-auto {{ $image->medical }}"></div>
-                                        <div class="col-10">Medical</div>
-                                    </div>
-                                </div>
-
                             </div>
                         </div>
-                    @endforeach
+                        @endforeach
                 @endif
                 </div>
             </div>
