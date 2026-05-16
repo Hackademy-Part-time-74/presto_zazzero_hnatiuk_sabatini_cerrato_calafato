206b3baa156e7a3e8ace73f96750d9dd90429cba
Ivanna <iwanozka666@gmail.com>
2026-05-16 09:14:46 +0200

    Fix nome file credenziali Google: rimosse 's' dal nome del file e aggiornato path nei job Vision

--- app/Jobs/GoogleVisionSafeSearch.php
+++ app/Jobs/GoogleVisionSafeSearch.php
@@ -35,7 +35,7 @@ class GoogleVisionSafeSearch implements ShouldQueue
 
         $image = file_get_contents(storage_path('app/public/' . $i->path));
 
-        putenv('GOOGLE_APPLICATION_CREDENTIALS=' .base_path('google_credentials.json'));
+        putenv('GOOGLE_APPLICATION_CREDENTIALS=' .base_path('google_credential.json'));
 
         $googleVisionClient = new ImageAnnotatorClient();
 
